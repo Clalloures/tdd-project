b72f42a1445530a7e8399ee51756a5554434cb6b
clalloures <clarissalloures@gmail.com>
2021-08-06 18:12:04 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 2

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -14,6 +14,47 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_can_start_a_list_for_one_user(self):
+
+        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
+        self.browser.get(self.live_server_url)
+        
+
+        # Ela nota que o título da página menciona TODO
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('To-Do', header_text)
+
+        # Ela é convidada a entrar com um item TODO imediatamente
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # Ela digita "Estudar testes funcionais" em uma caixa de texto
+        inputbox.send_keys('Estudar testes funcionais')
+
+        # Quando ela aperta enter, a página atualiza, e mostra a lista
+        # "1: Estudar testes funcionais" como um item da lista TODO
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+        
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        # Ela digita: "Estudar testes de unidade"
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox.send_keys('Estudar testes de unidade')
+        inputbox.send_keys(Keys.ENTER)
+
+        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+        self.wait_for_row_in_list_table('2: Estudar testes de unidade')
+        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+
+        # Ela visita a URL: a sua lista TODO ainda está armazenada
+
+        # Satisfeita, ela vai dormir
+
+
+
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Maria começa uma nova lista
         self.browser.get(self.live_server_url)
@@ -54,7 +95,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfeitos, ambos vão dormir
 
-
+        
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:  
