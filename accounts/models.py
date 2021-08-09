3790667bd5044fd1ec3b27d0e4fa8d905eb56eba
clalloures <clarissalloures@gmail.com>
2021-08-08 22:15:57 -0300

    Modelo de usuário mínimo

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin
 )
 
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    is_anonymous = False
+    is_authenticated = True
+
+    
 class Token(models.Model):
     email = models.EmailField()
     uid = models.CharField(max_length=255)
